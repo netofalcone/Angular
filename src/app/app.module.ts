fb5a8c1304362fcd14592d84df4304e460dec23c
Falcone <netofalcone@gmail.com>
2019-08-06 20:41:59 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import {InputTextModule} from 'primeng/components/inputtext/inputtext';
+import { AppComponent } from './app.component';
+import {ButtonModule} from 'primeng/components/button/button';
+import {TableModule} from 'primeng/components/table/table';
+import {TooltipModule} from 'primeng/components/tooltip/tooltip';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LancamentosPesquisaComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    TableModule,
+    TooltipModule
+
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
